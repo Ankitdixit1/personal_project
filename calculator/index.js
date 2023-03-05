6c41eba31fabc38256af14358e47601cc4d0a509
Anikt Dixit <ankitdixit29269@gmail.com>
2023-03-05 10:05:16 +0530

    adding code

--- calculator/index.js
+++ calculator/index.js
@@ -9,11 +9,14 @@ let buttonList = document.querySelectorAll(".button");
 Array.from(buttonList).forEach((sold)=>{
     sold.addEventListener("click", (e)=>{
         if(e.target.innerHTML == "="){
+            // let select = document.querySelector(".demo")
             collect = eval(collect);
-            document.querySelector("demo").value = collect;
+            document.querySelector(".demo").innerHTML = collect;
         }else{
             collect = collect + e.target.innerHTML;
-            document.querySelector("demo").value = collect;
+            document.querySelector(".demo").innerHTML = collect;
+            // soldd.innertext = collect;
+            // console.log(collect);
         }
     })
 })
