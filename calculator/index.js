7c7090857e50c3b8ea48eee7e5866fd780f4951c
Anikt Dixit <ankitdixit29269@gmail.com>
2023-03-06 08:35:19 +0530

    adidng code

--- calculator/index.js
+++ calculator/index.js
@@ -6,17 +6,17 @@
 // })
 let collect = "";
 let buttonList = document.querySelectorAll(".button");
-Array.from(buttonList).forEach((sold)=>{
+buttonList.forEach((sold)=>{
     sold.addEventListener("click", (e)=>{
         if(e.target.innerHTML == "="){
-            // let select = document.querySelector(".demo")
             collect = eval(collect);
+            document.querySelector(".demo").innerHTML = collect;
+        }else if(e.target.innerHTML == "AC"){
+            collect = "";
             document.querySelector(".demo").innerHTML = collect;
         }else{
             collect = collect + e.target.innerHTML;
             document.querySelector(".demo").innerHTML = collect;
-            // soldd.innertext = collect;
-            // console.log(collect);
         }
     })
 })
